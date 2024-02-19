b6d88928020effe8477edc00a2cdcc78b36e4e3c
schorio <schorioignace@gmail.com>
2024-02-19 06:41:51 +0300

    fonction qui affiche le synonyme d'un mot

---
+++ traitement_lexical/traitement.py
@@ -0,0 +1,20 @@
+import nltk
+import re
+from itertools import chain
+from nltk.corpus import wordnet as wn
+
+# Fonction qui affiche les synonymes d'un mot
+def synonyme(mot):
+    # Obtenir tous les synsets du mot
+    synsets = wn.synsets(mot)
+
+    # Fusionner les lemmes de tous les synsets et éliminer les doublons
+    synonyms = set(chain.from_iterable([synset.lemma_names() for synset in synsets]))
+
+    # Prendre les 5 premiers éléments
+    synonyms = list(synonyms)[:5]
+
+    # Joindre les synonymes en une seule chaîne séparée par des virgules
+    synonyms_str = ', '.join(synonyms)
+
+    print("\n", mot, " = ", synonyms_str)
