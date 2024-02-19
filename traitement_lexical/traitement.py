9a581974a2a6cb0b45cba09e5a7df8f40257040d
schorio <schorioignace@gmail.com>
2024-02-19 06:48:05 +0300

    fonction de traitement avec suppression des stopwords

--- traitement_lexical/traitement.py
+++ traitement_lexical/traitement.py
@@ -2,6 +2,8 @@ import nltk
 import re
 from itertools import chain
 from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+
 
 # Fonction qui affiche les synonymes d'un mot
 def synonyme(mot):
@@ -28,3 +30,28 @@ def definition(mot):
     # Afficher la definition du premier synonyme
     if synsets:
         print(mot, " : ", synsets[0].definition())
+        
+        
+# Fonction principal
+def traitement(nom_fichier): 
+    
+    # Charger les contenus d'un fichier texte
+    t_txt = open(nom_fichier, "r")
+    Text_txt = t_txt.read()
+    text_txt = (Text_txt).lower()
+    
+    # Importation des stopwords en anglais
+    stop_words = set(stopwords.words('english'))
+    
+    # Tokenisation
+    tokens = nltk.word_tokenize(text_txt)
+    tokens = [token for token in tokens if token not in stop_words]
+    
+    # supression du caractere speciaux de chaque mot du tableau
+    mots = []
+    for j in tokens:
+        text = re.sub(r"[^a-zA-Zéèçêù$âäëöôüû]","",j)
+        mots.append(text)
+        
+    # supression des emplacement libre dans le tableau
+    liste_mots = list(filter(lambda x: x!="",mots))
