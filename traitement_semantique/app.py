591b2bf3ec9b3de429d4b4e08c57e367f3b9de5f
schorio <schorioignace@gmail.com>
2024-02-22 06:57:53 +0300

    ajout d'un logo

--- traitement_semantique/app.py
+++ traitement_semantique/app.py
@@ -3,6 +3,8 @@ import pickle
 from sklearn.naive_bayes import GaussianNB
 from pdfminer.high_level import extract_text
 import customtkinter as ctk
+from PIL import Image, ImageTk
+from tkinter import Label
 
 # Load the model and vocabularies
 with open('datasets/models.gnb', 'rb') as f:
@@ -33,6 +35,12 @@ def prediction(chemin):
 root = ctk.CTk(fg_color="black")
 root.title("FilTra")
 root.geometry("400x720")
+
+image = Image.open('datasets/logo.png')
+image = image.resize((250, 250))
+photo = ImageTk.PhotoImage(image)
+bouton = Label(root, image=photo, borderwidth=0)
+bouton.pack()
 
 
 # Start the main event loop
