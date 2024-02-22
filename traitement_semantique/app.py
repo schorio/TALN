ab32380c27780788259ce80c1f8cdd6c71cd1e7e
schorio <schorioignace@gmail.com>
2024-02-22 06:51:30 +0300

    fonction qui predit la classe à qui appartient un texte d'un pdf

--- traitement_semantique/app.py
+++ traitement_semantique/app.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 from sklearn.naive_bayes import GaussianNB
+from pdfminer.high_level import extract_text
 
 # Load the model and vocabularies
 with open('datasets/models.gnb', 'rb') as f:
@@ -8,3 +9,16 @@ with open('datasets/models.gnb', 'rb') as f:
 
 with open("datasets/vocabulaires.txt", "r") as fichier:
     vocabulaires = [ligne.strip() for ligne in fichier.readlines()]
+    
+
+# Function to predict the category of a given PDF file
+def prediction(chemin):
+    prediction = []
+    texte = extract_text(chemin)
+    
+    for vocabulaire in vocabulaires:
+        prediction.append(1 if vocabulaire in texte else  0)
+
+    resultat = gnb.predict([prediction])
+    
+    return resultat[0]
